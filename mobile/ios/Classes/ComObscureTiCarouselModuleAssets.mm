d504343ac1e0c2ad271cd2b21d97a5cccfab93d1
Paul Mietz Egli <paul@obscure.com>
2012-05-10 08:43:49 -0700

    generated module files

---
+++ mobile/ios/Classes/ComObscureTiCarouselModuleAssets.mm
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComObscureTiCarouselModuleAssets.h"
+
+extern NSData* filterData(NSString* thedata, NSString* identifier);
+
+@implementation ComObscureTiCarouselModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
