550cde3a1cfd6ecd07a3042a68dbf9ba45223f8b
Paul Mietz Egli <paul@obscure.com>
2012-05-17 18:47:58 -0700

    updated example code

--- mobile/ios/example/app.js
+++ mobile/ios/example/app.js
@@ -1,69 +1,24 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
+var win = Ti.UI.createWindow();
+var base = Ti.UI.createWindow();
 
+var nav = Ti.UI.iPhone.createNavigationGroup({
+  window: base
+});
+win.add(nav);
 
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white',
-	layout: 'vertical',
+var data = [
+  { title: 'Transform Types', module: require('./types') },
+  { title: 'Custom Transform', module: require('./custom') },
+];
+
+var table = Ti.UI.createTableView({
+  data: data
 });
 
-var TiCarousel = require('com.obscure.TiCarousel');
-Ti.API.info("module is => " + TiCarousel);
-
-var label = Ti.UI.createLabel({ text: 'status' });
-win.add(label);
-
-var views = [];
-views.push(Ti.UI.createView({ height: 60, width: 60, backgroundColor: 'blue' }));
-views.push(Ti.UI.createView({ height: 60, width: 60, backgroundColor: 'yellow' }));
-views.push(Ti.UI.createView({ height: 60, width: 60, backgroundColor: 'red' }));
-views.push(Ti.UI.createView({ height: 60, width: 60, backgroundColor: 'green' }));
-views.push(Ti.UI.createView({ height: 60, width: 60, backgroundColor: 'gray' }));
-
-
-// build a custom transform matrix
-var f = function(offset) {
-  var dx = offset * 68.0,
-      dy = -70.0,
-      dz = -400.0;
-  
-  if (Math.abs(offset) < 1.0) {
-    dy = dy * Math.abs(offset);
-    dz = dz * Math.abs(offset);
-  }
-  
-  return [
-    { type: 'translate', values: [dx, dy, dz] }
-  ];
-};
-
-var carousel = TiCarousel.createCarouselView({
-  carouselType: TiCarousel.CAROUSEL_TYPE_CUSTOM,
-  backgroundColor: '#ccc',
-  width: Ti.UI.FILL,
-  height: 200,
-  itemWidth: 68,
-  numberOfVisibleItems: 5,
-  itemTransformForOffset: f,
+table.addEventListener('click', function(e) {
+  nav.open(e.row.module.createWindow());
 });
-carousel.addEventListener('scroll', function(e) {
-  label.text = String.format('scrolled to %d', e.currentIndex)
-})
-win.add(carousel);
 
-win.addEventListener('open', function(e) {
-  carousel.setViews(views);
-  carousel.reloadData();
-  carousel.scrollToIndex(3);
-})
+base.add(table);
 
 win.open();
-
-/*
-setTimeout(function() {
-  carousel.scrollToIndex(3);
-}, 5000);
-*/
